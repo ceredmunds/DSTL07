beda3ca9e1eab89dcfedc0ff3623b9b3ae457e9c
ceredmunds <ceredmunds@gmail.com>
2021-02-24 17:02:39 +0000

    Changed categorisation to button response

--- experimentCode/experiment/experiment.js
+++ experimentCode/experiment/experiment.js
@@ -1,5 +1,6 @@
 /* participant based variables */
 var displayCondition = 'separated' // integrated or separated
+var socialCondition = 'operator' // operator or superior
 
 /* category learning variables */
 var maxNumberCategoryLearningTrials = 2
@@ -55,7 +56,7 @@ var category_learning_instructions1 = {
   choices: ['space'],
   data: { test_part: "category learning instructions" }
 }
-timeline.push(category_learning_instructions1)
+// timeline.push(category_learning_instructions1)
 
 var craft_instructions = {
   type: "html-keyboard-response",
@@ -73,7 +74,7 @@ var craft_instructions = {
   choices: ['space'],
   data: { test_part: "category learning instructions" }
 }
-timeline.push(craft_instructions)
+// timeline.push(craft_instructions)
 
 var type_instructions = {
   type: "html-keyboard-response",
@@ -92,7 +93,7 @@ var type_instructions = {
   choices: ['space'],
   data: { test_part: "category learning instructions" }
 }
-timeline.push(type_instructions)
+// timeline.push(type_instructions)
 
 var status_instructions = {
   type: "html-keyboard-response",
@@ -111,7 +112,7 @@ var status_instructions = {
   choices: ['space'],
   data: { test_part: "category learning instructions" }
 }
-timeline.push(status_instructions)
+// timeline.push(status_instructions)
 
 if (displayCondition == "integrated") {
   var direction_instructions = {
@@ -133,7 +134,7 @@ if (displayCondition == "integrated") {
     choices: ['space'],
     data: { test_part: "category learning instructions" }
   }
-  timeline.push(direction_instructions)
+  // timeline.push(direction_instructions)
 } else if (displayCondition == "separated") {
   var direction_instructions = {
     type: "html-keyboard-response",
@@ -152,7 +153,7 @@ if (displayCondition == "integrated") {
     choices: ['space'],
     data: { test_part: "category learning instructions" }
   }
-  timeline.push(direction_instructions)
+  // timeline.push(direction_instructions)
 
   var direction_instructions1 = {
     type: "html-keyboard-response",
@@ -171,7 +172,7 @@ if (displayCondition == "integrated") {
     choices: ['space'],
     data: { test_part: "category learning instructions" }
   }
-  timeline.push(direction_instructions1)
+  // timeline.push(direction_instructions1)
 }
 
 var category_learning_instructions2 = {
@@ -189,37 +190,11 @@ var category_learning_instructions2 = {
   choices: ['space'],
   data: { test_part: "category learning instructions" }
 }
-timeline.push(category_learning_instructions2)
+// timeline.push(category_learning_instructions2)
 
-var finger_on_F = {
-  type: "html-keyboard-response",
-  stimulus: "<div style='height:80px;width:100%;background-color:#0f3273;position:absolute;top:0px;left:0px;'><img src='img/qmul/qm-logo-white.svg' alt='Queen Mary Header' style='width:200px;position:absolute;top:10px;left:10px;'></img></div>" +
-  "<div style='width:700px'>" +
-  "<p>Please put your left index finger on the <b>F key</b>.</p>" +
-  "<p>Remember, <b>F</b> is for <b>friendly</b>." +
-  "<div style='height:80px;width:100%;position:absolute;bottom:80px;left:0px;'><p>Please press the <b>F key</b> on your keyboard to continue.</p></div>" +
-  "<div style='height:80px;width:100%;background-color:#0f3273;position:absolute;bottom:0px;left:0px;'></div>",
-  choices: ['f'],
-  data: { test_part: "fingerF" }
-}
-timeline.push(finger_on_F)
-
-var finger_on_H = {
-  type: "html-keyboard-response",
-  stimulus: "<div style='height:80px;width:100%;background-color:#0f3273;position:absolute;top:0px;left:0px;'><img src='img/qmul/qm-logo-white.svg' alt='Queen Mary Header' style='width:200px;position:absolute;top:10px;left:10px;'></img></div>" +
-  "<div style='width:700px'>" +
-  "<p>Please put your left index finger on the <b>H key</b>.</p>" +
-  "<p>Remember, <b>H</b> is for <b>hostile</b>." +
-  "<div style='height:80px;width:100%;position:absolute;bottom:80px;left:0px;'><p>Please press the <b>H key</b> on your keyboard to continue.</p></div>" +
-  "<div style='height:80px;width:100%;background-color:#0f3273;position:absolute;bottom:0px;left:0px;'></div>",
-  choices: ['h'],
-  data: { test_part: "fingerH" }
-}
-timeline.push(finger_on_H)
-
-/* category learning trial */
-var category_learning_trial = {
-  type: 'category-learning',
+/* category learning phase */
+var categorisation_trial = {
+  type: 'category-button-response',
   displayCondition: displayCondition,
   dimension1: jsPsych.timelineVariable('dimension1'),
   dimension2: jsPsych.timelineVariable('dimension2'),
@@ -228,7 +203,8 @@ var category_learning_trial = {
   dimension5: jsPsych.timelineVariable('dimension5'),
   dimOrder: dimensionOrder,
   displayOrder : displayOrder,
-  choices: ['f', 'h'],
+  choices: ['Friendly', 'Hostile'],
+  response_ends_trial: true,
   data: {
     test_part: 'learning',
     abstract_category: jsPsych.timelineVariable('category')
@@ -244,7 +220,7 @@ var category_learning_trial = {
   }
 }
 
-var category_learning_feedback = {
+var feedback = {
   type: 'html-keyboard-response',
   stimulus: function () {
     var last_trial_correct = jsPsych.data.get().last(1).values()[0].correct
@@ -261,13 +237,14 @@ var category_learning_feedback = {
 }
 
 var category_learning_procedure = {
-  timeline: [category_learning_trial, category_learning_feedback],
+  timeline: [categorisation_trial, feedback],
   timeline_variables: category_learning_stimuli,
   randomize_order: true,
   loop_function: function () {
     // break loop if over trial limit
     var nTrials = jsPsych.data.get().filter({ test_part: "learning" }).count()
     if (nTrials >= maxNumberCategoryLearningTrials) {
+      // data.switch = "maxNoTrials"
       return false
     }
 
@@ -275,101 +252,102 @@ var category_learning_procedure = {
     var last_n_correct_trials = last_n_trials.filter({ correct: "true" })
     var accuracy = Math.round(last_n_correct_trials.count() / last_n_trials.count())
     if (accuracy < learningCriterion) {
+      // data.switch = "continue"
       return true // True to keep loop going
     } else {
+      // data.switch = "greaterThanLC"
       return false
     }
   }
 }
 timeline.push(category_learning_procedure)
 
-/* category learning trial */
-// var category_test_instructions = {
-//   type: "html-keyboard-response",
-//   stimulus: "<div style='height:80px;width:100%;background-color:#0f3273;position:absolute;top:0px;left:0px;'><img src='img/qmul/qm-logo-white.svg' alt='Queen Mary Header' style='width:200px;position:absolute;top:10px;left:10px;'></img></div>" +
-//   "<div style='width:700px'>" +
-//   "<p><b>Phase 2</b></p>" +
-//   "</div>" +
-//   "<div style='height:80px;width:100%;position:absolute;bottom:80px;left:0px;'><p>Please press the <b>space key</b> on your keyboard to begin.</p></div>" +
-//   "<div style='height:80px;width:100%;background-color:#0f3273;position:absolute;bottom:0px;left:0px;'></div>",
-//   choices: ['space'],
-//   data: { test_part: "category test instructions" },
-//   on_start: function () {
-//     document.getElementsByClassName("jspsych-content-wrapper")[0].style.color = "black"
-//     document.getElementsByClassName("jspsych-content-wrapper")[0].style.backgroundColor = "white"
-//   },
-//   on_finish: function () {
-//     document.getElementsByClassName("jspsych-content-wrapper")[0].style.backgroundColor = "#0102d1"
-//     document.getElementsByClassName("jspsych-content-wrapper")[0].style.color = "white"
-//   }
-// }
-// timeline.push(category_test_instructions)
-//
-// var category_test_trial = {
-//   type: "category-learning",
-//   condition: condition,
-//   dimension1: jsPsych.timelineVariable('dimension1'),
-//   dimension2: jsPsych.timelineVariable('dimension2'),
-//   dimension3: jsPsych.timelineVariable('dimension3'),
-//   dimension4: jsPsych.timelineVariable('dimension4'),
-//   dimension5: jsPsych.timelineVariable('dimension5'),
-//   labels: dimensionOrder,
-//   choices: ['f', 'h'],
-//   data: {
-//     test_part: "category_test_trial",
-//     dimLabels: dimensionOrder,
-//     abstract_category: jsPsych.timelineVariable('category')
-//   },
-//   on_finish: function (data) {
-//     data.correct_response = categoryLabels[data.abstract_category]
-//     if (data.key_press == jsPsych.pluginAPI.convertKeyCharacterToKeyCode(data.correct_response)) {
-//       data.correct = "true"
-//     } else {
-//       data.correct = "false"
-//     }
-//   },
-//   post_trial_gap: ITI
-// }
-//
-// var category_test_procedure = {
-//   timeline: [category_test_trial],
-//   timeline_variables: category_test_stimuli,
-//   repetitions: 1,
-//   randomize_order: true
-// }
-// timeline.push(category_test_procedure)
-//
-// /* Verbal report */
-// var verbal_report_instructions = {
-//   type: "html-keyboard-response",
-//   stimulus: "<div style='height:80px;width:100%;background-color:#0f3273;position:absolute;top:0px;left:0px;'><img src='img/qmul/qm-logo-white.svg' alt='Queen Mary Header' style='width:200px;position:absolute;top:10px;left:10px;'></img></div>" +
-//   "<div style='width:700px'>" +
-//   "<p><b>Verbal report</b></p>" +
-//   "<p>In the following, we will ask you some questions about how you completed this experiment.</p>" +
-//   "<p>Don't worry: there are absolutely no wrong answers!</p>" +
-//   "<p>Also, your answers to these questions WILL NOT influence whether you are paid.</p>" +
-//   "<p>Just try your best to explain as clearly and in as much detail as possible the answer.</p>" +
-//   "<p>Press any key to begin.</p>" +
-//   "</div>" +
-//   "<div style='height:80px;width:100%;position:absolute;bottom:80px;left:0px;'><p>Please press the <b>space key</b> on your keyboard to begin.</p></div>" +
-//   "<div style='height:80px;width:100%;background-color:#0f3273;position:absolute;bottom:0px;left:0px;'></div>",
-//   choices: ['space'],
-//   data: { test_part: "verbal report instructions" },
-//   on_start: function () {
-//     document.getElementsByClassName("jspsych-content-wrapper")[0].style.color = "black"
-//     document.getElementsByClassName("jspsych-content-wrapper")[0].style.backgroundColor = "white"
-//   }
-// }
-// timeline.push(verbal_report_instructions)
-//
-// var survey_trial = {
-//   type: 'survey-text',
-//   questions: [
-//     { prompt: "Please describe which stimulus dimension you thought was most important:", rows: 30, columns: 60, name: 'stimDimensionQ'}
-//   ],
-//   data: { test_part: "verbal report_trial" }
-// }
-// timeline.push(survey_trial)
+/* category test trials */
+var category_test_instructions = {
+  type: "html-keyboard-response",
+  stimulus: "<div style='height:80px;width:100%;background-color:#0f3273;position:absolute;top:0px;left:0px;'><img src='img/qmul/qm-logo-white.svg' alt='Queen Mary Header' style='width:200px;position:absolute;top:10px;left:10px;'></img></div>" +
+  "<div style='width:700px'>" +
+  "<p><b>Phase 1 Complete</b></p>" +
+  "<p>Congratulations, you've finished the first phase!</p>" +
+  "<p></p>" +
+  "</div>" +
+  "<div style='height:80px;width:100%;position:absolute;bottom:80px;left:0px;'><p>Please press the <b>space key</b> on your keyboard to continue.</p></div>" +
+  "<div style='height:80px;width:100%;background-color:#0f3273;position:absolute;bottom:0px;left:0px;'></div>",
+  choices: ['space'],
+  data: { test_part: "category test instructions" }
+}
+timeline.push(category_test_instructions)
+
+var conditionInstruction = ""
+if (socialCondition == 'operator') {
+  conditionInstruction = "<p>The confidence ratings are important, so your fellow operators know which craft to double check.</p>"
+} else if (socialCondition == 'superior') {
+  conditionInstruction = "<p>The confidence ratings are important, so your senior officer knows how to appropriately use the information.</p>"
+}
+
+var category_test_instructions1 = {
+  type: "html-keyboard-response",
+  stimulus: "<div style='height:80px;width:100%;background-color:#0f3273;position:absolute;top:0px;left:0px;'><img src='img/qmul/qm-logo-white.svg' alt='Queen Mary Header' style='width:200px;position:absolute;top:10px;left:10px;'></img></div>" +
+  "<div style='width:700px'>" +
+  "<p><b>Phase 2</b></p>" +
+  "<p>Here, you get to use the knowledge you've gained in an actual exercise.</p>" +
+  "<p>On each trial, you'll again have to categorise a craft as friendly or hostile.</p>" +
+  "<p>In addition, you'll be asked to rate your confidence in your categorisation with a slider.</p>" +
+  conditionInstruction +
+  "</div>" +
+  "<div style='height:80px;width:100%;position:absolute;bottom:80px;left:0px;'><p>Please press the <b>space key</b> on your keyboard when you are ready to start.</p></div>" +
+  "<div style='height:80px;width:100%;background-color:#0f3273;position:absolute;bottom:0px;left:0px;'></div>",
+  choices: ['space'],
+  data: { test_part: "category test instructions" }
+}
+timeline.push(category_test_instructions1)
+
+var confidence_rating = {
+  type: 'html-slider-response',
+  stimulus: 'How confident are you in your classification?',
+  labels: ['No idea', 'Certain'],
+  require_movement: true
+}
+
+var category_test_procedure = {
+  timeline: [categorisation_trial, confidence_rating],
+  timeline_variables: category_test_stimuli,
+  repetitions: 1,
+  randomize_order: true
+}
+timeline.push(category_test_procedure)
+
+/* Verbal report */
+var verbal_report_instructions = {
+  type: "html-keyboard-response",
+  stimulus: "<div style='height:80px;width:100%;background-color:#0f3273;position:absolute;top:0px;left:0px;'><img src='img/qmul/qm-logo-white.svg' alt='Queen Mary Header' style='width:200px;position:absolute;top:10px;left:10px;'></img></div>" +
+  "<div style='width:700px'>" +
+  "<p><b>Verbal report</b></p>" +
+  "<p>In the following, we will ask you some questions about how you completed this experiment.</p>" +
+  "<p>Don't worry: there are absolutely no wrong answers!</p>" +
+  "<p>Also, your answers to these questions WILL NOT influence whether you are paid.</p>" +
+  "<p>Just try your best to explain as clearly and in as much detail as possible the answer.</p>" +
+  "<p>Press any key to begin.</p>" +
+  "</div>" +
+  "<div style='height:80px;width:100%;position:absolute;bottom:80px;left:0px;'><p>Please press the <b>space key</b> on your keyboard to begin.</p></div>" +
+  "<div style='height:80px;width:100%;background-color:#0f3273;position:absolute;bottom:0px;left:0px;'></div>",
+  choices: ['space'],
+  data: { test_part: "verbal report instructions" },
+  on_start: function () {
+    document.getElementsByClassName("jspsych-content-wrapper")[0].style.color = "black"
+    document.getElementsByClassName("jspsych-content-wrapper")[0].style.backgroundColor = "white"
+  }
+}
+timeline.push(verbal_report_instructions)
+
+var survey_trial = {
+  type: 'survey-text',
+  questions: [
+    { prompt: "Please describe which stimulus dimension you thought was most important:", rows: 30, columns: 60, name: 'stimDimensionQ'}
+  ],
+  data: { test_part: "verbal report_trial" }
+}
+timeline.push(survey_trial)
 
 /* define debrief */
 // var debrief_block = {
---
+++ experimentCode/jspsych-6.2.0/plugins/jspsych-category-button-response.js
@@ -0,0 +1,318 @@
+/**
+ * jspsych-html-button-response
+ * Josh de Leeuw
+ *
+ * plugin for displaying a stimulus and getting a keyboard response
+ *
+ * documentation: docs.jspsych.org
+ *
+ **/
+
+jsPsych.plugins["category-button-response"] = (function() {
+
+  var plugin = {};
+
+  plugin.info = {
+    name: 'html-button-response',
+    description: '',
+    parameters: {
+      displayCondition: {
+        type: jsPsych.plugins.parameterType.STRING,
+        pretty_name: 'Condition',
+        default: null,
+        description: 'Condition: either separated or integrated.'
+      },
+      dimension1: {
+        type: jsPsych.plugins.parameterType.HTML_STRING,
+        pretty_name: 'Stimulus',
+        default: undefined,
+        description: 'The HTML string to be displayed'
+      },
+      dimension2: {
+        type: jsPsych.plugins.parameterType.HTML_STRING,
+        pretty_name: 'Stimulus',
+        default: undefined,
+        description: 'The HTML string to be displayed'
+      },
+      dimension3: {
+        type: jsPsych.plugins.parameterType.HTML_STRING,
+        pretty_name: 'Stimulus',
+        default: undefined,
+        description: 'The HTML string to be displayed'
+      },
+      dimension4: {
+        type: jsPsych.plugins.parameterType.HTML_STRING,
+        pretty_name: 'Stimulus',
+        default: undefined,
+        description: 'The HTML string to be displayed'
+      },
+      dimension5: {
+        type: jsPsych.plugins.parameterType.HTML_STRING,
+        pretty_name: 'Stimulus',
+        default: undefined,
+        description: 'The HTML string to be displayed'
+      },
+      dimOrder: {
+        type: jsPsych.plugins.parameterType.STRING,
+        array: true,
+        pretty_name: 'Labels',
+        default: jsPsych.ALL_KEYS,
+        description: 'Name of the labels for each dimension in order.'
+      },
+      displayOrder: {
+        type: jsPsych.plugins.parameterType.STRING,
+        array: true,
+        pretty_name: 'Labels',
+        default: jsPsych.ALL_KEYS,
+        description: 'Order that dimensions should display in separable condition.'
+      },
+      choices: {
+        type: jsPsych.plugins.parameterType.STRING,
+        pretty_name: 'Choices',
+        default: undefined,
+        array: true,
+        description: 'The labels for the buttons.'
+      },
+      button_html: {
+        type: jsPsych.plugins.parameterType.STRING,
+        pretty_name: 'Button HTML',
+        default: '<button class="jspsych-btn">%choice%</button>',
+        array: true,
+        description: 'The html of the button. Can create own style.'
+      },
+      prompt: {
+        type: jsPsych.plugins.parameterType.STRING,
+        pretty_name: 'Prompt',
+        default: null,
+        description: 'Any content here will be displayed under the button.'
+      },
+      stimulus_duration: {
+        type: jsPsych.plugins.parameterType.INT,
+        pretty_name: 'Stimulus duration',
+        default: null,
+        description: 'How long to hide the stimulus.'
+      },
+      trial_duration: {
+        type: jsPsych.plugins.parameterType.INT,
+        pretty_name: 'Trial duration',
+        default: null,
+        description: 'How long to show the trial.'
+      },
+      margin_vertical: {
+        type: jsPsych.plugins.parameterType.STRING,
+        pretty_name: 'Margin vertical',
+        default: '0px',
+        description: 'The vertical margin of the button.'
+      },
+      margin_horizontal: {
+        type: jsPsych.plugins.parameterType.STRING,
+        pretty_name: 'Margin horizontal',
+        default: '8px',
+        description: 'The horizontal margin of the button.'
+      },
+      response_ends_trial: {
+        type: jsPsych.plugins.parameterType.BOOL,
+        pretty_name: 'Response ends trial',
+        default: true,
+        description: 'If true, then trial will end when user responds.'
+      }
+    }
+  }
+
+  plugin.trial = function(display_element, trial) {
+
+    // Get stimulus to display
+    var stimulus = ''
+    // Get craft
+    var craft = eval("trial.dimension" + trial.dimOrder[0])
+    if (craft == 0) {
+      craftHtml = '<img src="img/stimuli/air.png"></img>'
+      var yPos = "0px"
+    } else if (craft == 1) { // submarine
+      craftHtml = '<img src="img/stimuli/submarine.png"></img>'
+      var yPos = "-30px" // edit to shift decoy to correct place
+    }
+    // Get type
+    type = eval("trial.dimension" + trial.dimOrder[1])
+    if (type == 0) {
+      typeHtml = '<img src="img/stimuli/autonomous.png"></img>'
+    } else if (type == 1) {
+      typeHtml = '<img src="img/stimuli/decoy.png"></img>'
+    }
+    // Get status
+    status = eval("trial.dimension" + trial.dimOrder[2])
+    if (status == 0) {
+      statusHtml = '<img src="img/stimuli/unharmed.png"></img>'
+    } else if (status == 1) {
+      statusHtml = '<img src="img/stimuli/damaged.png"></img>'
+    }
+    // Get speed
+    speed = eval("trial.dimension" + trial.dimOrder[3])
+    // Get direction
+    direction = eval("trial.dimension" + trial.dimOrder[4])
+
+    if (trial.displayCondition == 'separated') {
+      // Get speed html
+      if (speed == 0) {
+        speedHtml = '<img src="img/stimuli/slow.png"></img>'
+      } else if (speed == 1) {
+        speedHtml = '<img src="img/stimuli/fast.png"></img>'
+      }
+      // Get direction html
+      if (direction == 0) {
+        directionHtml = '<img src="img/stimuli/left.png"></img>'
+      } else if (direction == 1) {
+        directionHtml = '<img src="img/stimuli/right.png"></img>'
+      }
+      // Randomise order of presentation (per participant)
+      stimulusHtml = [craftHtml, typeHtml, statusHtml, speedHtml, directionHtml]
+      const displayHtml = trial.displayOrder.map(i => stimulusHtml[i])
+      stimulus = '<div class="row">' +
+      '<div class="column">' + displayHtml[0] + '</div>' +
+      '<div class="column">' + displayHtml[1] + '</div>' +
+      '<div class="column">' + displayHtml[2] + '</div>' +
+      '</div>' +
+      '<div class="row">' +
+      '<div class="shift">' + displayHtml[3] + '</div>' +
+      '<div class="column">' + displayHtml[4] + '</div>' +
+      '</div>'
+
+      var html = '<div id="jspsych-html-button-response-stimulus">' + stimulus + '</div>'
+    } else if (trial.displayCondition == 'integrated') {
+      var xPos = "0px" // offset for slow submarine to compensate for badly drawn stimulus
+      // Get spped and direction html
+      if (speed==0 & direction==0) {
+        speedDirectionHtml = '<img src="img/stimuli/slow_left.png"></img>'
+        if (craft == 1) {
+          xPos = "-15px"
+        }
+      } else if (speed==1 & direction==0) {
+        speedDirectionHtml = '<img src="img/stimuli/fast_left.png"></img>'
+      } else if (speed==0 & direction==1) {
+        speedDirectionHtml = '<img src="img/stimuli/slow_right.png"></img>'
+        if (craft == 1) {
+          xPos = "15px"
+        }
+      } else if (speed==1 & direction==1) {
+        speedDirectionHtml = '<img src="img/stimuli/fast_right.png"></img>'
+      }
+      displayHtml = [speedDirectionHtml, craftHtml, typeHtml, statusHtml] // don't need to randomise order in this condition
+      stimulus = '<div class="background">' +
+      '<div class="layer" style="left:' + xPos + ';">' + displayHtml[0] + '</div>' +
+      '<div class="layer">' + displayHtml[1] + '</div>' +
+      '<div class="layer" style="top:' + yPos + ';">' + displayHtml[2] + '</div>' + // edit to shift decoy to correct place
+      '<div class="layer">' + displayHtml[3] + '</div>' +
+      '</div>'
+      var html = '<div id="jspsych-html-button-response-stimulus">' + stimulus + '</div>'
+    } else {
+      stimulus = 'WARNING: UNDEFINED CONDITION'
+      // Display stimulus
+      var html = '<div id="jspsych-html-button-response-stimulus">' + 'WARNING: UNDEFINED CONDITION' + '</div>'
+    }
+
+    // display buttons
+    var buttons = [];
+    if (Array.isArray(trial.button_html)) {
+      if (trial.button_html.length == trial.choices.length) {
+        buttons = trial.button_html;
+      } else {
+        console.error('Error in html-button-response plugin. The length of the button_html array does not equal the length of the choices array');
+      }
+    } else {
+      for (var i = 0; i < trial.choices.length; i++) {
+        buttons.push(trial.button_html);
+      }
+    }
+    html += '<div id="jspsych-html-button-response-btngroup">';
+    for (var i = 0; i < trial.choices.length; i++) {
+      var str = buttons[i].replace(/%choice%/g, trial.choices[i]);
+      html += '<div class="jspsych-html-button-response-button" style="display: inline-block; margin:'+trial.margin_vertical+' '+trial.margin_horizontal+'" id="jspsych-html-button-response-button-' + i +'" data-choice="'+i+'">'+str+'</div>';
+    }
+    html += '</div>';
+
+    // show prompt if there is one
+    if (trial.prompt !== null) {
+      html += trial.prompt;
+    }
+    display_element.innerHTML = html;
+
+    // start time
+    var start_time = performance.now();
+
+    // add event listeners to buttons
+    for (var i = 0; i < trial.choices.length; i++) {
+      display_element.querySelector('#jspsych-html-button-response-button-' + i).addEventListener('click', function(e){
+        var choice = e.currentTarget.getAttribute('data-choice'); // don't use dataset for jsdom compatibility
+        after_response(choice);
+      });
+    }
+
+    // store response
+    var response = {
+      rt: null,
+      button: null
+    };
+
+    // function to handle responses by the subject
+    function after_response(choice) {
+
+      // measure rt
+      var end_time = performance.now();
+      var rt = end_time - start_time;
+      response.button = parseInt(choice);
+      response.rt = rt;
+
+      // after a valid response, the stimulus will have the CSS class 'responded'
+      // which can be used to provide visual feedback that a response was recorded
+      display_element.querySelector('#jspsych-html-button-response-stimulus').className += ' responded';
+
+      // disable all the buttons after a response
+      var btns = document.querySelectorAll('.jspsych-html-button-response-button button');
+      for(var i=0; i<btns.length; i++){
+        //btns[i].removeEventListener('click');
+        btns[i].setAttribute('disabled', 'disabled');
+      }
+
+      if (trial.response_ends_trial) {
+        end_trial();
+      }
+    };
+
+    // function to end trial when it is time
+    function end_trial() {
+
+      // kill any remaining setTimeout handlers
+      jsPsych.pluginAPI.clearAllTimeouts();
+
+      // gather the data to store for the trial
+      var trial_data = {
+        "rt": response.rt,
+        "stimulus": trial.stimulus,
+        "button_pressed": response.button
+      };
+
+      // clear the display
+      display_element.innerHTML = '';
+
+      // move on to the next trial
+      jsPsych.finishTrial(trial_data);
+    };
+
+    // hide image if timing is set
+    if (trial.stimulus_duration !== null) {
+      jsPsych.pluginAPI.setTimeout(function() {
+        display_element.querySelector('#jspsych-html-button-response-stimulus').style.visibility = 'hidden';
+      }, trial.stimulus_duration);
+    }
+
+    // end trial if time limit is set
+    if (trial.trial_duration !== null) {
+      jsPsych.pluginAPI.setTimeout(function() {
+        end_trial();
+      }, trial.trial_duration);
+    }
+
+  };
+
+  return plugin;
+})();
